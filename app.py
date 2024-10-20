f811416c7c10a6f2cc4b0859a0faa2f7a629b5bb
samiribrh <samiribrahimov2277@gmail.com>
2024-10-20 14:31:29 +0400

    Extra sample data count

--- app.py
+++ app.py
@@ -42,7 +42,21 @@ def add_sample_data():
         light1 = Light(id=1, status='on')
         light2 = Light(id=2, status='on')
         light3 = Light(id=3, status='on')
-        db.session.add_all([light1, light2, light3])
+        light4 = Light(id=4, status='on')
+        light5 = Light(id=5, status='on')
+        light6 = Light(id=6, status='on')
+        light7 = Light(id=7, status='on')
+        light8 = Light(id=8, status='on')
+        light9 = Light(id=9, status='on')
+        light10 = Light(id=10, status='on')
+        light11 = Light(id=11, status='on')
+        light12 = Light(id=12, status='on')
+        light13 = Light(id=13, status='on')
+        light14 = Light(id=14, status='on')
+        light15 = Light(id=15, status='on')
+        light16 = Light(id=16, status='on')
+        db.session.add_all([light1, light2, light3, light4, light5, light6, light7, light8,
+                            light9, light10, light11, light12, light13, light14, light15, light16])
         db.session.commit()
 
     # Add sample water systems
