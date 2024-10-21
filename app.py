0536b79b0d49b99aa1096f069f3f303d107d761b
samiribrh <samiribrahimov2277@gmail.com>
2024-10-21 12:03:46 +0400

    Cleaner code

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
-import click
-
 from flask import Flask
 from flask_cors import CORS
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_migrate import Migrate
-from flask.cli import with_appcontext
 
 from api.light import light_bp
 from api.water import water_bp
+from core.commands import add_sample_data
 from database.db import db
-from models.light import Light
-from models.water import Water
-
 
 app = Flask(__name__)
 CORS(app)
@@ -33,47 +28,9 @@ app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 app.register_blueprint(light_bp)
 app.register_blueprint(water_bp)
 
-
-@click.command(name='add_sample_data')
-@with_appcontext
-def add_sample_data():
-    # Add sample lights
-    if not Light.query.first():
-        light1 = Light(id=1, status='on')
-        light2 = Light(id=2, status='on')
-        light3 = Light(id=3, status='on')
-        light4 = Light(id=4, status='on')
-        light5 = Light(id=5, status='on')
-        light6 = Light(id=6, status='on')
-        light7 = Light(id=7, status='on')
-        light8 = Light(id=8, status='on')
-        light9 = Light(id=9, status='on')
-        light10 = Light(id=10, status='on')
-        light11 = Light(id=11, status='on')
-        light12 = Light(id=12, status='on')
-        light13 = Light(id=13, status='on')
-        light14 = Light(id=14, status='on')
-        light15 = Light(id=15, status='on')
-        light16 = Light(id=16, status='on')
-        db.session.add_all([light1, light2, light3, light4, light5, light6, light7, light8,
-                            light9, light10, light11, light12, light13, light14, light15, light16])
-        db.session.commit()
-
-    # Add sample water systems
-    if not Water.query.first():
-        water1 = Water(id=1, status='on')
-        water2 = Water(id=2, status='on')
-        db.session.add_all([water1, water2])
-        db.session.commit()
-
-    print("Sample data added to the database.")
+app.cli.add_command(add_sample_data)  # type: ignore
 
 
-# Register the command with the Flask app
-app.cli.add_command(add_sample_data)
-
-
-# Routes
 @app.route("/")
 def index():
     return "Welcome to Smart Office!"
--- main.cpp
+++ main.cpp
@@ -4,13 +4,21 @@
 const char* ssid = "Holberton - Students";
 const char* password = "HBTNStuds24";
 
-int relayPin = 7;
-bool switchState = false;
+int relayPinWater = 3;
+int relayPinLight = 2;
+
+bool switchStateWater = false;
+bool switchStateLight = false;
+
+WiFiClient wifiClient;
 
 void setup() {
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW);
-  
+  pinMode(relayPinWater, OUTPUT);
+  pinMode(relayPinLight, OUTPUT);
+
+  digitalWrite(relayPinWater, LOW);
+  digitalWrite(relayPinLight, LOW);
+
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
@@ -18,45 +26,63 @@ void setup() {
   {
     delay(1000);
   }
+
+  Serial.println("Connected to WiFi");
 }
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED)
   {
-    HTTPClient http;
-    http.begin("172.17.208.1");
-    int httpCode = http.GET();
 
-    if (httpCode > 0)
-    {
-      String payload = http.getString();
-      Serial.println(payload);
+    checkState("172.17.221.251:5000/waterstate", relayPinWater, switchStateWater);
 
-      if (payload.indexOf("true") > -1)
-      {
-        switchState = true;
-      }
-      else
-      {
-        switchState = false;
-      }
 
-      if (switchState)
-      {
-        digitalWrite(relayPin, HIGH);
-      }
-      else
-      {
-        digitalWrite(relayPin, LOW);
-      }
-    } 
-    else
-    {
-      Serial.println("Error on HTTP request");
-    }
-
-    http.end();
+    checkState("172.17.221.251:5000/lightstate", relayPinLight, switchStateLight);
+  }
+  else
+  {
+    Serial.println("WiFi not connected");
   }
 
   delay(5000);
+}
+
+void checkState(const char* url, int relayPin, bool& switchState) {
+  HTTPClient http;
+  http.begin(wifiClient, url);
+  int httpCode = http.GET();
+
+  if (httpCode > 0)
+  {
+    String payload = http.getString();
+    Serial.print("Payload from ");
+    Serial.println(url);
+    Serial.println(payload);
+
+    if (payload.indexOf("on") > -1)
+    {
+      switchState = true;
+    }
+    else
+    {
+      switchState = false;
+    }
+
+    if (switchState)
+    {
+      digitalWrite(relayPin, HIGH);
+      Serial.println("Switching ON relay");
+    }
+    else
+    {
+      digitalWrite(relayPin, LOW);
+      Serial.println("Switching OFF relay");
+    }
+  }
+  else
+  {
+    Serial.println("Error on HTTP request");
+  }
+
+  http.end();
 }
